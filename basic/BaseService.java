3f320c4645d7b86753ad054738b9bbed5b67b7a3
Dougest <15050055260@163.com>
2018-09-28 11:16:14 +0800

    super classes and You can customize

--- basic/BaseService.java
+++ basic/BaseService.java
@@ -9,27 +9,15 @@ import java.util.Map;
  * @Date 2018年9月28日 上午10:44:12
  */
 public interface BaseService<T> {
-	/** 根据id删除表数据 **/
-	int deleteByPrimaryKey(String id);
+	int delete(Long id);
 
-	/** 插入实体数据到表 **/
-	int insert(T record);
+	int add(String data);
 
-	/** 插入局部实体数据到表 **/
-	int insertSelective(T record);
+	T info(Long id);
 
-	/** 主键查询实体数据 **/
-	T selectByPrimaryKey(String id);
+	int update(String data);
 
-	/** 根据主键更新局部实体数据 **/
-	int updateByPrimaryKeySelective(T record);
+	String commonList(int pageIndex, int pageSize, String data);
 
-	/** 根据主键更新实体数据 **/
-	int updateByPrimaryKey(T record);
-
-	/** 解析传入的data数据,分页查询 **/
-	String commonList(String data, int pageIndex, int pageSize);
-
-	/** 根据插入的数据查询列表 **/
 	List<T> queryList(Map<String, Object> map);
 }
